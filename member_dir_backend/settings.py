6c0ef39e89f0ab8afbf18b3315c42590f23bca39
AugustinBrain <austinbrain25@gmail.com>
2025-07-20 13:09:20 +0800

    Railway Deployment v4

--- member_dir_backend/settings.py
+++ member_dir_backend/settings.py
@@ -77,7 +77,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,6 +87,8 @@ MIDDLEWARE = [
 
     "corsheaders.middleware.CorsMiddleware",
 ]
+
+
 
 ROOT_URLCONF = 'member_dir_backend.urls'
 
@@ -170,6 +172,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
