f356e50b1af4b1be169d8272ab7d4307d5b23bf8
Ra1g0 <jrmmabaet2022@plm.edu.ph>
2025-07-22 11:50:41 +0800

    nagdagdag lang

--- member_dir_backend/settings.py
+++ member_dir_backend/settings.py
@@ -39,7 +39,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["https://directorybackend-production.up.railway.app", "http://127.0.0.1:8000", "http://localhost:8000"]
+CSRF_TRUSTED_ORIGINS = ["https://directorybackend-production.up.railway.app", "http://127.0.0.1:8000", "http://localhost:8000", "http://localhost:5173"]
 
 # REST_FRAMEWORK = {
 #     "DEFAULT_AUTHENTICATION_CLASSES": (
